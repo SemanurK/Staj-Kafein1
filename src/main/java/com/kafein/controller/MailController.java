23e4de2e8aeead353a097c84b014274d469f99c3
SemanurK <sema.174k@gmail.com>
2020-03-26 13:56:50 +0200

    MailController güncellendi

--- src/main/java/com/kafein/controller/MailController.java
+++ src/main/java/com/kafein/controller/MailController.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
@@ -14,19 +16,30 @@ import javax.mail.search.SearchTerm;
 
 import java.io.*;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.kafein.entity.TalepDao;
 import com.kafein.model.MailModel;
+import com.kafein.model.TalepTablo;
 
 @Controller
 @RequestMapping("/Mail")
 public class MailController {
 
 	static List<MailModel> list = new ArrayList<MailModel>();
+	static List<TalepTablo> list2 = new ArrayList<TalepTablo>();
 	static String result = "";
+	@Autowired
+	TalepDao dao;
 
 	public static void check(String host, String storeType, String user, String password) {
 		try {
@@ -46,72 +59,12 @@ public class MailController {
 
 			SearchTerm sender = new FromTerm(new InternetAddress("sema.174k@gmail.com"));
 			Message[] messages = emailFolder.search(sender);
-
-			// Message[] messages = emailFolder.getMessages();
 			System.out.println("messages.length---" + messages.length);
 			int n = messages.length;
-
 			for (int i = 0; i < n; i++) {
 
 				Message message = messages[i];
-
-				System.out.println("---------------------------------");
-				System.out.println("Email Number " + (i + 1));
-
-				System.out.println("Subject: " + message.getSubject());
-
-				System.out.println("From: " + message.getFrom()[0]);
-
 				writePart(message);
-				list.add(new MailModel(i + 1, message.getFrom()[0].toString(), user, message.getSubject(),result));
-
-				// String line = reader.readLine();
-				// if ("YES".equals(line)) {
-				// message.writeTo(System.out);
-				// } else if ("QUIT".equals(line)) {
-				// break;
-				// }
-				// if (message.isMimeType("text/plain")) {
-				// System.out.println("Text: " +
-				// message.getContent().toString());
-				//
-				// } else if (message.isMimeType("multipart/*")) {
-				// String result = "";
-				// MimeMultipart mimeMultipart = (MimeMultipart)
-				// message.getContent();
-				//
-				// int count = mimeMultipart.getCount();
-				// for (int j = 0; j < count; j++) {
-				// BodyPart bodyPart = mimeMultipart.getBodyPart(j);
-				// if (bodyPart.isMimeType("text/plain")) {
-				// result = result + "\n" + bodyPart.getContent();
-				// break; // without break same text appears twice in
-				// // my tests
-				// } else if (bodyPart.isMimeType("text/html")) {
-				// String html = (String) bodyPart.getContent();
-				// result = result + "\n" + Jsoup.parse(html).text();
-				//
-				// }
-				// else if(bodyPart.isMimeType("multipart/*")){
-				//
-				// for(int k=0; k<count; k++)
-				// {
-				// Multipart mp = (Multipart) bodyPart.getContent();
-				// if (bodyPart.isMimeType("text/plain")) {
-				// result = result + "\n" + bodyPart.getContent();
-				// break; // without break same text appears twice in
-				// // my tests
-				// } else if (bodyPart.isMimeType("text/html")) {
-				// String html = (String) bodyPart.getContent();
-				// result = result + "\n" + Jsoup.parse(html).text();
-				//
-				// }
-				// }
-				// }
-				// }
-				// System.out.println("Text: " + result);
-				//
-				// }
 			}
 
 			// close the store and folder objects
@@ -126,55 +79,54 @@ public class MailController {
 	}
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String main(ModelMap model) {
+	public String main(ModelMap model,final RedirectAttributes redirectAttributes) {
 
 		list.clear();
 		String host = "pop.gmail.com";// change accordingly
 		String mailStoreType = "imap";
 		String username = "kafeinxx8@gmail.com";// change accordingly
-		String password = "123456789k.";// change accordingly
+		String password = "174kafein*";// change accordingly
 		check(host, mailStoreType, username, password);
-		model.addAttribute("mailliste", list);
-		return "/Mail/index";
+		
+			int yeni=dao.listkontrol_sprint(list2);
+			model.addAttribute("taleplist", dao.getTaleps());
+			redirectAttributes.addFlashAttribute("yenitalepsayisi", yeni);
+		
+		
+		
+		return "redirect:/Talep/index";
+		
 
 	}
 
 	public static void writePart(Part p) throws Exception {
-		if (p instanceof Message)
-			// Call methos writeEnvelope
-			writeEnvelope((Message) p);
-	
-		System.out.println("----------------------------");
-		System.out.println("CONTENT-TYPE: " + p.getContentType());
+		// result="";
 
-		// check if the content is plain text
 		if (p.isMimeType("text/plain")) {
-			System.out.println("This is plain text");
-			System.out.println("---------------------------");
-			System.out.println((String) p.getContent());
-			result = result + "\n" + (String) p.getContent();
-			System.out.println("Message : "+result);
+			if (result != null) {
+				result = "";
+				result = result + (String) p.getContent();
+			} else
+				result = result + (String) p.getContent();
 
 		}
 		// check if the content has attachment
 		else if (p.isMimeType("multipart/*")) {
-			System.out.println("This is a Multipart");
-			System.out.println("---------------------------");
+
 			Multipart mp = (Multipart) p.getContent();
 
 			int count = mp.getCount();
 			for (int i = 0; i < count; i++)
 				writePart(mp.getBodyPart(i));
 		}
-		// check if the content is a nested message
+
 		else if (p.isMimeType("message/rfc822")) {
-			System.out.println("This is a Nested Message");
-			System.out.println("---------------------------");
+
 			writePart((Part) p.getContent());
 		}
-		// check if the content is an inline image
+
 		else if (p.getContentType().contains("image/")) {
-			System.out.println("content type" + p.getContentType());
+
 			File f = new File("image" + new Date().getTime() + ".jpg");
 			DataOutputStream output = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(f)));
 			com.sun.mail.util.BASE64DecoderStream test = (com.sun.mail.util.BASE64DecoderStream) p.getContent();
@@ -186,47 +138,58 @@ public class MailController {
 		} else {
 			Object o = p.getContent();
 			if (o instanceof String) {
-				System.out.println("This is a string");
-				System.out.println("---------------------------");
-				System.out.println((String) o);
-			} else if (o instanceof InputStream) {
-				System.out.println("This is just an input stream");
-				System.out.println("---------------------------");
-				InputStream is = (InputStream) o;
-				is = (InputStream) o;
-				int c;
-				while ((c = is.read()) != -1)
-					System.out.write(c);
-			} else {
-				System.out.println("This is an unknown type");
-				System.out.println("---------------------------");
-				System.out.println(o.toString());
+				table((String) o);
+
 			}
 		}
 
 	}
 
-	public static void writeEnvelope(Message m) throws Exception {
-		System.out.println("This is the message envelope");
-		System.out.println("---------------------------");
-		Address[] a;
+	public static void table(String html ) {
+		Document doc2 = Jsoup.parse(html);
+		Elements tableElements = doc2.select("table");
+		//String deneme=tableElements.toString();
+		if (!tableElements.toString().equals("")) {
+			Elements tableRowElements = tableElements.select(":not(thead) tr");
+			// Sprint no alma
+			String str = result;
 
-		// FROM
-		if ((a = m.getFrom()) != null) {
-			for (int j = 0; j < a.length; j++)
-				System.out.println("FROM: " + a[j].toString());
+			String sprint_sentence = null;
+			Pattern p = Pattern.compile("[A-Z](?i)[^.?!]*?\\b(Sprint|Planlama|Raporu)\\b[^.?!]*[.?!]");
+			Matcher m = p.matcher(str);
+			
+				while (m.find()) {
+					if (m.group().contains("Planlama")) {
+						if (m.group().contains("Raporu")) {
+							sprint_sentence = m.group();
+							 break;
+						}
+					}
+				}
+				
+				System.out.println(sprint_sentence.toString());
+				String [] sprint_reserve=sprint_sentence.split(" ");
+				int sprint_no=Integer.parseInt(sprint_reserve[1]);					
+
+			
+
+			for (int i = 1; i < tableRowElements.size() - 1; i++) {
+				Element row = tableRowElements.get(i);
+				// System.out.println("row");
+				Elements rowItems = row.select("td");
+				String rowItems2 = rowItems.select("table").toString();
+				if (rowItems2.equals("")) {
+
+					 list2.add(new TalepTablo(rowItems.get(0).text(),
+					 rowItems.get(2).text().replace('\'', ' '),
+					 rowItems.get(5).text(),sprint_no));
+				} else
+					break;
+				
+			}
+			 System.out.println();
+
 		}
-
-		// TO
-		if ((a = m.getRecipients(Message.RecipientType.TO)) != null) {
-			for (int j = 0; j < a.length; j++)
-				System.out.println("TO: " + a[j].toString());
-		}
-
-		// SUBJECT
-		if (m.getSubject() != null)
-			System.out.println("SUBJECT: " + m.getSubject());
-
 	}
 
 }
